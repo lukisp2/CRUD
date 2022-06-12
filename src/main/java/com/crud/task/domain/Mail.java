a14c201c95d4628818809a9f3dac145b216bfd8a
lukisp2 <lukaszmarchelelk@gmail.com>
2022-06-12 12:34:17 +0200

    Commit zadania 19.3

--- src/main/java/com/crud/task/domain/Mail.java
+++ src/main/java/com/crud/task/domain/Mail.java
@@ -1,12 +1,17 @@
 package com.crud.task.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Optional;
+
 @Getter
+@Builder
 @AllArgsConstructor
 public class Mail {
     public final String mailTo;
+    public final Optional<String> toCc;
     public final String subject;
     public final String message;
 }
---
+++ src/test/java/com/crud/task/service/SimpleEmailServiceTest.java
@@ -0,0 +1,47 @@
+package com.crud.task.service;
+
+import com.crud.task.domain.Mail;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        //Given
+        Mail mail = Mail.builder()
+                .mailTo("test@test.com")
+                .message("Test message")
+                .subject("Subject Test")
+                .toCc(Optional.empty())
+                .build();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        //When
+        simpleEmailService.send(mail);
+        //Then
+        Assertions.assertEquals(Optional.empty(),mail.getToCc());
+        verify(javaMailSender,times(1)).send(mailMessage);
+
+    }
+}
