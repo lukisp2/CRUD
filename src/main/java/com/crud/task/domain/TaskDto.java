eda6b26240d41821c7c5b4bad27d8538fe79e740
lukisp2 <lukaszmarchelelk@gmail.com>
2022-07-18 18:13:35 +0200

    Commit do heroku zmiana NoArgsConstructor

--- src/main/java/com/crud/task/domain/TaskDto.java
+++ src/main/java/com/crud/task/domain/TaskDto.java
@@ -1,10 +1,13 @@
 package com.crud.task.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
